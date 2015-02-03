37c5ace9b1a8bf0e57ac3bf712872ae794b6d835
bnicenboim <bruno.nicenboim@gmail.com>
2015-02-03 19:26:10 +0100

    Major changes in the simulation of data

--- lmm.R
+++ lmm.R
@@ -5,26 +5,43 @@ library(MASS)
 
 #############
 ### Simulation of typical dataset of psycholinguistics
-### For simplicity of the code not in latin-square
+### in latin-square
 N_subj <- 40
 N_item <- 20
-N_coef <- 4  #number of predictors
+N_coef <- 4  #number of coeficients for a 2x2 design:
+            # 1 + beta1 X1 + beta2 X2 + beta3 X1 X2
+#we assume that the -1000/DV is normally distributed (as typically happens with RT)
+#2 conditions with 2 levels:
+(coefs <- c(-1000/500, -1000/540+1000/500, -1000/540+1000/500, -1000/550+1000/500))
 
-(means <- c(-1000/550, -1000/500,-1000/540, -1000/480))
-#-1.818182 -2.000000 -1.851852 -2.083333
+sdev_subj <- c(.9,.1,.1,.01) 
+sdev_item <- c(.2,.01,.01,.01) # 
+sdev <- .3                   # sigma in bayesian model
+rho_subjs <- .25
+rho_items <- .02
+
+#so that each item includes every combination (no latin square):
+
+conditions <- data.frame(
+  c1=rep(c("a","b"),(N_item+1)*N_subj/4),
+    c2=rep(c("c","d"),(N_item+1),each=N_item/4),r=rep(1:(N_item+1),N_subj) )
+conditions<-conditions[conditions$r!=(N_item+1),c("c1","c2")]
+
+contrasts(conditions$c1) <- contr.sum(2)
+contrasts(conditions$c2) <- contr.sum(2)
+
+X <- model.matrix(~1+c1*c2,data=conditions)
 
 # Correlation matrix by subj
-rho_subj<- matrix(rep(.2,N_coef*N_coef),nrow=N_coef)
+rho_subj<- matrix(rep(rho_subjs,N_coef*N_coef),nrow=N_coef)
 diag(rho_subj) <-1
 
 # Correlation matrix by item
-rho_item<- matrix(rep(.02,N_coef*N_coef),nrow=N_coef)
+rho_item<- matrix(rep(rho_items,N_coef*N_coef),nrow=N_coef)
 diag(rho_item) <-1
 
 
-sdev_subj <- c(.7,.05,.05,.05) 
-sdev_item <- c(.1,.01,.01,.01) # 
-sdev <- .05                    # sigma in bayesian model
+
 
 b_subj <- sdev_subj %*% t(sdev_subj)  
 Sigma_subj <- b_subj * rho_subj  #variance covariance matrix for subj (Sigma_u in bayesian model)
@@ -33,47 +50,55 @@ raneff_subj <- mvrnorm(n = N_subj, rep(0,N_coef), Sigma_subj)
 
 b_item <- sdev_item %*% t(sdev_item)  
 Sigma_item <- b_item * rho_item  #variance covariance matrix for item (Sigma_w in bayesian model)
-raneff_item <- mvrnorm(n = N_item, rep(0,N_coef), Sigma_subj)
+raneff_item <- mvrnorm(n = N_item, rep(0,N_coef), Sigma_item)
 
-datawide <- data.frame(subj =factor(rep(seq(1:N_subj),each=N_item)),
+
+#auxiliary matrixes of 1 for the random effects:
+subjones <- matrix(rep( c(rep(1,N_item),rep(0,N_item*N_subj)),N_subj),
+    nrow=N_subj *N_item,ncol=N_subj )
+
+tempones<-rep(c(rep( c(1,rep(0,N_subj-1)),N_item),0),N_item)
+itemones <- matrix(tempones[1:(length(tempones)-N_item)],
+    nrow=N_item *N_subj,ncol=N_item )
+
+
+datalong <- data.frame(subj =factor(rep(seq(1:N_subj),each=N_item)),
                     item =factor(rep(seq(1:N_item),times=N_subj)),
                     trialN=factor(seq(1:N_subj*N_item)),
-                    conditions=
-matrix(rep(means,times=N_subj*N_item ),nrow=N_subj*N_item,byrow=T)+
-raneff_subj[rep(1:nrow(raneff_subj),each=N_item),]+
-raneff_item[rep(1:nrow(raneff_item),times=N_subj),])
-
-head(datawide,100)
+                    conditions,
+                    rt= round(-1000/ rnorm(N_subj*N_item,
+                        X %*% matrix(coefs)+
+                    rowSums(X %*% t(raneff_subj) * subjones)+
+                    rowSums(X %*% t(raneff_item) * itemones)
+                    , sdev) ,0)
+                    )
+head(datalong,10)
 library(reshape2)
-datalong <- melt(datawide, variable.name="condition",
-    value.name="rrt")
 
-head(datalong)
-dcast(datalong, condition~.,mean)
+datalong[datalong$rt <0,]$rt <- 150
+
+dcast(datalong, c1+c2~.,function(x) mean(-1000/x))
+dcast(datalong, c1+c2~.,mean)
 
 ######################################3
 ###
 ### Analysis with lmer
 library(lme4)
 
-# model with 4 levels
-contrasts(datalong$condition) <- contr.sum(4)
-summary(m1 <- lmer(rrt ~ condition + (1|subj) +(1|item), datalong))
 
-# 2x2 model
-datalong$c1 <- factor(ifelse(datalong$condition %in% c('conditions.1','conditions.2'),"A","B"))
-datalong$c2 <- factor(ifelse(datalong$condition %in% c('conditions.1','conditions.3'),"X","Y"))
 
-contrasts(datalong$c1) <- contr.sum(2)
-contrasts(datalong$c2) <- contr.sum(2)
+summary(m1 <- lmer(I(-1000/rt) ~ c1*c2 + (1|subj) +(1|item), datalong))
 
-summary(m2 <- lmer(rrt ~ c1*c2 + (1|subj) +(1|item), datalong))
+
 
 
 # 2x2 model with full random-effects structures
 
-#summary(m2_full <- lmer(rrt ~ c1*c2 + (c1*c2|subj) +(c1*c2|item), datalong))
-#It didn't converge with the simulated data from above.
+summary(m2_full <- lmer(I(-1000/rt) ~ c1*c2 + (c1*c2|subj) +(c1*c2|item), datalong))
+
+#It fails to converge
+
+
 
 #################################3
 ## Bayesian LMM with RStan
@@ -82,7 +107,7 @@ library(rstan)
 stan_lmm <- stan_model(file="lmm.stan") 
 
 
-niter <- 500  #should be more than 2000
+niter <- 3000
 nchains <- 4
 
 subj <- as.numeric(as.character(datalong$subj))
@@ -90,10 +115,10 @@ N_subj <- length(unique(subj))
 item <-as.numeric(as.character(datalong$item))
 N_item <- length(unique(item))
 
-rt <-datalong$rrt
+rt <- -1000/datalong$rt
 
-# Model equivalent to m1 lmer(rrt ~ condition + (1|subj) +(1|item), datalong)
-x_1 <- model.matrix(~ 1+ condition , data=datalong)
+# Model equivalent to m1 lmer(rrt ~ c1*c2 + (1|subj) +(1|item), datalong)
+x_1 <- model.matrix(~ 1+ c1*c2 , data=datalong)
 x_u_1 <- model.matrix(~ 1, data=datalong) #for subj
 x_w_1 <-  model.matrix(~ 1, data=datalong) #for item
 
@@ -121,43 +146,13 @@ samples_lmm_1 <- sampling(stan_lmm,
 
 
 
-print(samples_lmm_1,pars=c("beta","sigma_u","sigma_w","sigma","Cor_u","Cor_w"))
+#print(samples_lmm_1,pars=c("beta","sigma_u","sigma_w","sigma","Cor_u","Cor_w"))
 
+sum_lmm1 <- summary(samples_lmm_1,pars=c("beta","sigma_u","sigma_w","sigma","Cor_u","Cor_w"),probs = c(0.025,  0.975) ,digits_summary = 3)
 
+#summary in table:
+round(sum_lmm1$summary,2)
 
-
-# Model equivalent to m2 lmer(rrt ~ c1*c2 + (1|subj) +(1|item), datalong)
-x_2 <- model.matrix(~ 1+ c1*c2 , data=datalong)
-x_u_2 <- model.matrix(~ 1, data=datalong) #for subj
-x_w_2 <-  model.matrix(~ 1, data=datalong) #for item
-
-
-lsdata <- list(rt=rt, 
-        subj=subj,
-        item=item,
-        N_obs=nrow(datalong),
-        N_coef=ncol(x_2),
-        N_coef_u=ncol(x_u_2),
-        N_coef_w=ncol(x_w_2),
-        x =x_2,
-        x_u=x_u_2,
-        x_w=x_w_2,
-        N_subj=N_subj,
-        N_item=N_item
-) 
-
-
-
-
-samples_lmm_2 <- sampling(stan_lmm,    
-                data=lsdata, 
-                iter=niter,
-                chains=nchains
-                )
-
-
-
-print(samples_lmm_2,pars=c("beta","sigma_u","sigma_w","sigma","Cor_u","Cor_w"))
 
 
 
@@ -193,4 +188,10 @@ samples_lmm_full <- sampling(stan_lmm,
 
 
 
-print(samples_lmm_full,pars=c("beta","sigma_u","sigma_w","sigma","Cor_u","Cor_w"))
+# print(samples_lmm_full,pars=c("beta","sigma_u","sigma_w","sigma","Cor_u","Cor_w"))
+
+sum_lmm_full<- summary(samples_lmm_full,pars=c("beta","sigma_u","sigma_w","sigma","Cor_u","Cor_w"),probs = c(0.025,  0.975) ,digits_summary = 3)
+
+#summary in table:
+round(sum_lmm_full$summary,2)
+
